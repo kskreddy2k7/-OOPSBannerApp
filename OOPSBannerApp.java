61d3f452a8391e304aa9bc6840495aec880d2f6b
Kata Sai Kranthu Reddy <kskreddy2k7@gmail.com>
2026-02-17 09:28:47 +0530

    Implement OOPSBannerApp with main method
    Add documentation comments and main method to display OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,1 +1,22 @@
 
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ *
+ * This class demonstrates a simple Java application that displays
+ * the Object-Oriented Programming System (OOPS) acronym to the console.
+ *
+ * @author Kskreddy
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the Java application
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Print OOPS to the console
+        System.out.println("OOPS");
+    }
+}
