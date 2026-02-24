2ece874c8e476e7fcd0b9ed84a11d481d85cfab7
Kata Sai Kranthu Reddy <kskreddy2k7@gmail.com>
2026-02-24 10:01:02 +0530

    Refactor banner printing in OOPSBannerApp
    Refactor OOPSBannerApp to print OOPS banner using an array and loop.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,68 +1,8 @@
- feature/UC1-PrintOOPS
-
-
 public class OOPSBannerApp {
 
-  
     public static void main(String[] args) {
 
-       
         System.out.println("OOPS");
 
-public class OOPSBannerApp {
-
-    public static void main(String[] args) {
-
-        // Store all banner lines inside an array
-        String[] banner = {
-
-                String.join(" ",
-                        "  *****  ",
-                        "  *****  ",
-                        "  ****** ",
-                        "  *****  "),
-
-                String.join(" ",
-                        " *     * ",
-                        " *     * ",
-                        " *     * ",
-                        " *     * "),
-
-                String.join(" ",
-                        " *     * ",
-                        " *     * ",
-                        " *     * ",
-                        " *       "),
-
-                String.join(" ",
-                        " *     * ",
-                        " *     * ",
-                        " ******  ",
-                        "  *****  "),
-
-                String.join(" ",
-                        " *     * ",
-                        " *     * ",
-                        " *       ",
-                        "       * "),
-
-                String.join(" ",
-                        " *     * ",
-                        " *     * ",
-                        " *       ",
-                        " *     * "),
-
-                String.join(" ",
-                        "  *****  ",
-                        "  *****  ",
-                        " *       ",
-                        "  *****  ")
-        };
-
-        // Use loop instead of multiple print statements
-        for (String line : banner) {
-            System.out.println(line);
-        }
-        main
     }
 }
