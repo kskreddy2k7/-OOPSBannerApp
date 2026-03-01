285c6c8b388b62734045ccbc6014795c0894f463
Kata Sai Kranthu Reddy <kskreddy2k7@gmail.com>
2026-03-01 22:39:06 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,1 @@
 
-public static void main(String[] args) {
-
-    System.out.println("  *****   *****   ******   *****  ");
-    System.out.println(" *     * *     *  *     *  *      ");
-    System.out.println(" *     * *     *  *     *  *      ");
-    System.out.println(" *     * *     *  ******   *****  ");
-    System.out.println(" *     * *     *  *        *      ");
-    System.out.println(" *     * *     *  *        *      ");
-    System.out.println("  *****   *****   *        *****  ");
-}
